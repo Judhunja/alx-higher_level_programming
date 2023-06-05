23d39df09e4f992a38e85165ddcca41ff185e355
Judhunja <judxhunja@gmail.com>
2023-06-05 21:26:08 +0300

    printing out strings whose lengths have been formatted

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = f"{word:s}"[:3]
 word_last_2 = f"{word:s}"[-2:]
 middle_word = f"{word:s}"[1:8]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print(f"First 3 letters: {word_first_3:s}")
+print(f"Last 2 letters: {word_last_2:s}")
+print(f"Middle word: {middle_word:s}")
