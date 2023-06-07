a107ff0386816ca21bddd9871867baa66fa7dd72
Judhunja <judxhunja@gmail.com>
2023-06-07 15:11:28 +0300

    update index slicing to slice off the first and last words of the string

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = f"{word:s}"[:3]
 word_last_2 = f"{word:s}"[-2:]
-middle_word = f"{word:s}"[-8:8]
+middle_word = f"{word:s}"[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
