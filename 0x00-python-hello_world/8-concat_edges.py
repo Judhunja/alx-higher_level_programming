c240a09befa411ee597ceedeab182d00e69c0d56
Judhunja <judxhunja@gmail.com>
2023-06-07 15:41:16 +0300

    printing sections of a string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = f"{str:s}"[39:67] + f"{str:s}"[107:112] + f"{str:s}"[:6]
+ language that combines remarkable power with very clear syntax"[39:67][107:112][:6]
 print(str)
