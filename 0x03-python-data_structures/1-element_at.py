1f2d7905bef70b65ad11fd6af844eb1fa8c4c8cc
Judhunja <judxhunja@gmail.com>
2023-06-12 16:09:54 +0300

    retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list:
-        if idx < 0:
-            return None
-        elif idx > len(my_list):
-            return None
-        else:
-            return my_list[idx]
+    if len(my_list) == 0:
+        return (None)
+    elif idx < 0:
+        return (None)
+    elif idx > len(my_list):
+        return (None)
+    else:
+        return my_list[idx]
 
 
 if __name__ == "__main__":
