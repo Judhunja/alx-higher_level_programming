ea6e168364eb8f12fe0d1ec76995b57afa7232d5
Judhunja <judxhunja@gmail.com>
2023-06-14 22:34:45 +0300

    prints a dictionary by ordered keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for x, v in sorted(a_dictionary.items()):
-        print(f"{x}: {v}")
+    if a_dictionary:
+        for x, v in sorted(a_dictionary.items()):
+            print(f"{x}: {v}")
 
 
 if __name__ == "__main__":
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for x in a_dictionary:
+        a_dictionary[key] = value
+
+
+if __name__ == "__main__":
+    update_dictionary(a_dictionary, key, value)
