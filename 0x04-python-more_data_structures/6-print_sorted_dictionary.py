8aed9726e9d46c94af0d7824391ba9d06e3dfea2
Judhunja <judxhunja@gmail.com>
2023-06-14 23:03:43 +0300

    replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,3 +4,7 @@ def print_sorted_dictionary(a_dictionary):
         for x, v in sorted(a_dictionary.items()):
             print(f"{x}: {v}")
 
+
+
+if __name__ == "__main__":
+    print_sorted_dictionary(a_dictionary)
