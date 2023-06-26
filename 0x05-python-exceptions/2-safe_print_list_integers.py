802595d15de2eb5a2b4c86dc8af1f4d88d35092a
Judhunja <judxhunja@gmail.com>
2023-06-26 23:15:10 +0300

    printing x elements of a list only if they are integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for y in range(x):
+            if isinstance(my_list[y], int):
+                print("{:d}".format(my_list[y]), end='')
+                count += 1
+
+    except IndexError:
+        pass
+
+    finally:
+        print("")
+        return count
+
+
+if __name__ == "__main__":
+    safe_print_list_integers(my_list=[], x=0)
