40c67e88097eb01082dec1e294cffc23fe3b5d81
Judhunja <judxhunja@gmail.com>
2023-06-27 21:44:33 +0300

    an empty class 'Square' that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square:
+    """ defines a class square
+    """
+    def __init__(self):
+        """initializes class Square
+        args:
+            none
+        """
+        pass
+    def square(self):
+        """initializes object square
+        args:
+            square: a square
+        """
+        self.square = square
