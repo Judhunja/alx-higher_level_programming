d8245dc2f7af712c8fa7d8099ec7c3a55a1efeca
Judhunja <judxhunja@gmail.com>
2023-07-03 23:04:21 +0300

    using unittest module to perform tests

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for max integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+""" This module contains a class TestMaxInt """
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Initializes the class TestMaxInteger
+    Inherits Testcase from the unittest module
+    Contains tests for the max int function
+    """
+
+    def test_success_1(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([4, 5, 6]), 6)
+
+    def test_return_none(self):
+        self.assertEqual(max_integer([]), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
