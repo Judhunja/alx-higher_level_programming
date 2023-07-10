844393edb3c429b9a68976fa2023b802b8a449e3
Judhunja <judxhunja@gmail.com>
2023-07-11 02:51:29 +0300

    Rectangle with attributes width and height which are validated by integer_validator inherited from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module contains a BaseGeometry class and a class
+    Rectangle that inherits from BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Initializes a BseGeometry class """
+    def area(self):
+        """ Raises an exception """
+        raise Exception("area() is not implemeted")
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ Initializes a class Rectangle """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
