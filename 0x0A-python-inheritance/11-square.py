12cbfa4e42f29a757b662f1093a430bc585f6ddc
Judhunja <judxhunja@gmail.com>
2023-07-11 03:35:45 +0300

    printing size and area of a Square class that inherits from Rectangle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -41,11 +41,11 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
     """ initializes a class Square """
     def __init__(self, size):
-        self.integer_validator("size", size)
         self.__size = size
+        self.integer_validator("size", size)
 
     def area(self):
         return self.__size ** 2
 
     def __str__(self):
-        return f"[Rectangle] {self.__size}/{self.__size}"
+        return f"[Square] {self.__size}/{self.__size}"
