ae4ee63327307fd2e5e2b75e2a4599a0fd2e3f46
Judhunja <judxhunja@gmail.com>
2023-07-12 04:31:39 +0300

    (reload_from_json)replaces attributes of Student instance with values stored in a dict

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This module contains a Student class """
+
+
+class Student:
+    """ Initializes a Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dict representation of a Student instance,
+        if attrs is a list of strings only attribute names
+        contained in the list must be retrieved
+        """
+        if isinstance(attrs, list) and attrs is not None:
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
+        else:
+            return vars(self)
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance with
+        values from a dictionary
+
+        Args:
+            json: a dictionary containing attributes and values
+        """
+        for attribute, value in json.items():
+            setattr(self, attribute, value)
