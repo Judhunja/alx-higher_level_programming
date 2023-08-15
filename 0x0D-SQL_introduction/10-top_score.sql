a1a719f13b2e738e2fa85b4092d97f7e05f49b9c
Judhunja <judxhunja@gmail.com>
2023-08-15 18:47:56 +0300

    select rows and order by desc

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of "second_table" in this order: score, name
+-- the scores are ordered in descending order so DESC is used
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
