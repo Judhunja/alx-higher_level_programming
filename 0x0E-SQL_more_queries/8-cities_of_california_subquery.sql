0d2e61bbef43e4e53f1d7ed80f3e07ca6df44009
Judhunja <judxhunja@gmail.com>
2023-08-16 21:06:14 +0300

    lists all records whose foreign keys match

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- lists all cities of California in the database "hbtn_0d_usa"
 -- selects only the cities where the state is California
-USE hbtn_0d_usa;
 SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 GROUP BY id
