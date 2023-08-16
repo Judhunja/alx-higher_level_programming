efd81386a8245a3057257111bb594dbd4e70ca20
Judhunja <judxhunja@gmail.com>
2023-08-17 02:38:23 +0300

    excluding specific values after selecting them

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- lists all genres not linked to 'Dexter'
+-- uses subquery to exclude matching columns
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN(
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name;
