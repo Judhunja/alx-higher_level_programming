fe034751abc633c7735249e29ead1e361985224b
Judhunja <judxhunja@gmail.com>
2023-08-16 18:58:49 +0300

    creates user and grants only the select privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates a database "hbtn_0d_2" and the user "user_0d_2"
+-- user_0d_2 is given only the SELECT priviledge in the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+GRANT SELECT
+ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
