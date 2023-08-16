0142da64dea3dc3f04f068e7b21245605da7a018
Judhunja <judxhunja@gmail.com>
2023-08-16 18:37:48 +0300

    creates user and grants all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates user "user_0d_1"
+-- grants all privileges to this user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
