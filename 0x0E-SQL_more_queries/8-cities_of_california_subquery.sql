04ebfe66ed212245b864ed2af43ff848ba9a398c
Judhunja <judxhunja@gmail.com>
2023-08-16 21:06:56 +0300

    lists all records whose foreign keys match

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,4 @@
 -- selects only the cities where the state is California
 SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-GROUP BY id
 ORDER BY id ASC;
