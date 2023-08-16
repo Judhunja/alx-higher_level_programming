37f7adb02028a4fdb1a80b9a8cda7dfa953b61de
Judhunja <judxhunja@gmail.com>
2023-08-16 17:45:01 +0300

    lists privileges of users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all priviledges of users "user_0d_1" and "user_od_2"
+-- all the grants for the users will be shown
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
