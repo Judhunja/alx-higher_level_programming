29d0cb530b6474010da8040984455f50512db30b
Judhunja <judxhunja@gmail.com>
2023-08-16 21:15:39 +0300

    joining tables in a database

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities contained in "hbtn_0d_usa"
+-- displays cities.id - cities.name - state.name
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states
+ORDER BY cities.id ASC;
