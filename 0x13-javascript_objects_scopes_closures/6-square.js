b5a3512e785545d1f69a1e063b7853ff043e3726
Judhunja <judxhunja@gmail.com>
2023-09-12 17:15:42 +0300

    printing 'X' if c is undefined

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,9 +4,9 @@ const Square1 = require('./5-square');
 
 module.exports = class Square extends Square1 {
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.height; i++) {
       let row = '';
-      for (let j = 0; j < this.size; j++) {
+      for (let j = 0; j < this.width; j++) {
         if (c !== undefined) {
           row += c;
         } else {
