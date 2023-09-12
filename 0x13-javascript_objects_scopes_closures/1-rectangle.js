c6648d4b66e048ab1d96aaf585dea1f793e9766d
Judhunja <judxhunja@gmail.com>
2023-09-12 14:08:09 +0300

    initializing class attributes and exporting the class

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-	width;
-	height;
-	constructor(w, h) {
-		this.width = w;
-		this.height = h;
-	}
-}
+  width;
+  height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
