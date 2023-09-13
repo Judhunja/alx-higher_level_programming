3b289d9eff592d5c3d7ef9eb1775fa04fe58ea4a
Judhunja <judxhunja@gmail.com>
2023-09-13 20:37:24 +0300

    high order function returning a closure

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-	return function to_string(number) {
-		return number.toString(base);
-	}
-}
+  return function toString (number) {
+    return number.toString(base);
+  };
+};
