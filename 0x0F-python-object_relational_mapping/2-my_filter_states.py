8e4f7ee6e4aa8e77dd05353e03fd8a8ebf2d42a1
Judhunja <judxhunja@gmail.com>
2023-09-15 21:56:59 +0300

    displays all values in the database table that match the name argument passed

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" This module contains a function select """
+
+from sys import argv
+import MySQLdb
+
+
+def select():
+    """ This function selects states from a database that
+    match a certain parameter """
+
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(host='localhost',
+                         user=username,
+                         passwd=password,
+                         db=database_name,
+                         port=3306)
+
+    curs = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = '{}'".format(state_name)
+
+    curs.execute(query)
+
+    results = curs.fetchall()
+
+    for state in results:
+        print(state)
+
+    curs.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    select()
