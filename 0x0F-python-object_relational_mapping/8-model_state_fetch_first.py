83bcb7016982b724ea4530ef5193aa4064a26174
Judhunja <judxhunja@gmail.com>
2023-09-16 17:14:17 +0300

    fetching the first query result using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" prints the first State object from a database """
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}\
+            @localhost:3306/{database_name}")
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    result = session.query(State).order_by(State.id).first()
+
+    print(f"{result.id}: {result.name}")
