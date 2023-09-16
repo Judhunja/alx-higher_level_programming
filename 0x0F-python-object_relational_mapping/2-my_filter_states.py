9afe4c35e3c29d8dc1e0c3e3cf0b199fc9da72c1
Judhunja <judxhunja@gmail.com>
2023-09-16 12:13:45 +0300

    filtering name using like binary which is case insensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,8 +22,8 @@ def select():
 
     curs = db.cursor()
 
-    query = "SELECT \
-            * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = "SELECT * FROM states \
+            WHERE BINARY name LIKE '{}' ORDER BY id".format(state_name)
 
     curs.execute(query)
 
