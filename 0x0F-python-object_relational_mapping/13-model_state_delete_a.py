6e9b4145c553aedc444994b5dc86ab8bc094a6b2
Judhunja <judxhunja@gmail.com>
2023-09-16 22:40:59 +0300

    iterates through a list of objects and deletes them using session.delete()

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     if results is not None:
         for result in results:
             session.delete(result)
-            session.commit()
+
+    session.commit()
 
     session.close()
