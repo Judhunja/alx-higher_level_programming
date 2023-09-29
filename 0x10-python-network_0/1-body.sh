376fb46b37d041266f728936a662e49d6ea7db4b
Judhunja <judxhunja@gmail.com>
2023-09-29 15:48:55 +0300

    printing the body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in an URL, sends a GET request to that URL and displays the body of the response
-curl -s -w "%{http_code}" "$1" | awk -v RS=200 '/200/ {print $2}'
+curl -s -w "\n%{http_code}" "$1" | awk -v RS=200 '/200/ {print $2}'
