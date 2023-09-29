71d4fbbdbc7c285af705cbac7945ccb4d946eacc
Judhunja <judxhunja@gmail.com>
2023-09-30 01:46:33 +0300

    posting using requests with added email variable

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This script prints the X-Request-Id in the response
+header after a request has been made """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    post = requests.post(argv[1], data={'email': argv[2]})
+
+    post.encoding = 'utf-8'
+
+    print(post)
