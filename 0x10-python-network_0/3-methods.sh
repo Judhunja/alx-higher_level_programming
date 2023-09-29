0ddd1b6bfe035daf16fe30ed393fcffeaf03f122
Judhunja <judxhunja@gmail.com>
2023-09-29 16:52:35 +0300

    -I to only get the head of the response

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | grep -i 'allow' | cut -d ':' -f 2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'
+curl -X OPTIONS -sI "$1" | grep -i 'allow' | cut -d ':' -f 2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'
