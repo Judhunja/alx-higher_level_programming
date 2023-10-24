3d054c94661a3171ad103b30aa06e83cae0b89a1
Judhunja <judxhunja@gmail.com>
2023-10-24 22:35:27 +0300

    reading from a file using fs

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(data);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,3 @@
+# 0x14. JavaScript - Web scraping
+Using request and fetching api's
+reading and writing to a file using fs
