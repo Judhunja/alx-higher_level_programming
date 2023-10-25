cfb8c7a1d449fa834b343de542ba3149ce7dde01
Judhunja <judxhunja@gmail.com>
2023-10-25 20:46:34 +0300

    using regex to match id regardless of api endpoint

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,8 +11,11 @@ request(url, function (error, response, body) {
     const results = data.results;
     let x = 0;
     for (const char of results) {
-      if (char.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        x += 1;
+      for (const charUrl of char.characters) {
+        const urlSplit = charUrl.match(/\/(\d+)\/$/);
+        if (urlSplit && urlSplit[1] === '18') {
+          x++;
+        }
       }
     }
     console.log(x);
